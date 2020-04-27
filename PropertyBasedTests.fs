61f7de6f3ac3526fe16f7558146991568bf27727
cboudereau <clement.boudereau@availpro.com>
2020-04-27 11:21:41 +0200

    clean

--- PropertyBasedTests.fs
+++ PropertyBasedTests.fs
@@ -5,31 +5,31 @@ open FsCheck
 open FsCheck.Xunit
 
 module Sand = 
-    let randomList = 
-        let one = 
-            gen { 
-                let! drums = 
-                    Gen.elements [ 1m .. 1m .. 10m ]
-                    |> Gen.listOfLength 1 //Change this parameter to 2 to get the issue
-                    |> Gen.map (List.map (fun x -> { Type=Sand; Size = PositiveSize.get x }))
-                let totalSize = drums |> List.fold (fun x y -> x + y.Size) PositiveSize.Zero
-                let container = Container.empty totalSize Set.empty
-                return container, drums }
+    type FullContainer = 
+        static member Values() = 
+            let fullFilledContainers count = 
+                let fullFilledContainer = 
+                    gen { 
+                        let! drums = 
+                            Gen.elements [ 1m .. 1m .. 10m ]
+                            |> Gen.listOfLength 1 //Change this parameter to 2 to get the issue
+                            |> Gen.map (List.map (fun x -> { Type=Sand; Size = PositiveSize.get x }))
+                        let totalSize = drums |> List.fold (fun x y -> x + y.Size) PositiveSize.Zero
+                        let container = Container.empty totalSize Set.empty
+                        return container, drums }
 
-        let list count = 
-            List.init count (fun _ -> one)
-            |> Gen.sequence
+                gen {
+                    let! countainersCount = count
+                    return! List.init countainersCount (fun _ -> fullFilledContainer) |> Gen.sequence 
+                }
 
-        gen {
-            let! countainersCount = Gen.choose (1, 2)
-            let! fullContainers = list countainersCount 
-            let (containers, drums) = fullContainers |> List.fold (fun (containers, drums) (c,d) -> c::containers, List.append drums d) ([],[]) 
-            let! containers = Gen.shuffle containers
-            let! drums = Gen.shuffle drums
-            return drums |> Array.toList, containers |> Array.toList
-        }
-
-    type FullContainer = static member Values() = Arb.fromGen randomList
+            gen {
+                let! fullContainers = Gen.choose (1, 2) |> fullFilledContainers 
+                let (containers, drums) = fullContainers |> List.fold (fun (containers, drums) (c,d) -> c::containers, List.append drums d) ([],[]) 
+                let! containers = Gen.shuffle containers
+                let! drums = Gen.shuffle drums
+                return drums |> Array.toList, containers |> Array.toList
+            } |> Arb.fromGen 
 
     let [<Property (Arbitrary=[| typeof<FullContainer> |])>] ``Given drums which fit exactly in containers, when packing them, then expect all containers full of drums`` (drums, containers) =
         let test drums containers = 
