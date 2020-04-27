678364b380bd2fd9afe81cc3467556ebb6cb95dc
cboudereau <clement.boudereau@availpro.com>
2020-04-27 09:41:27 +0200

    clean

--- Core.fs
+++ Core.fs
@@ -8,4 +8,5 @@ module Core
         let get x = match x with Ok x' -> x' | e -> failwithf "expected a Ok result, got %A" e
 
     module Operators = 
+        //https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/symbol-and-operator-reference/
         let (==) actual expected = shouldBe expected actual
