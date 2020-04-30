61249d831716003e69bce4518ca9f4b50d316c32
cboudereau <clement.boudereau@availpro.com>
2020-04-30 12:06:32 +0200

    clean

--- src/UnitTests.fs
+++ src/UnitTests.fs
@@ -1,16 +1,21 @@
-﻿namespace ``Given drums and incompatible containers, `` 
-    open Xunit
-    open Domain
-    open Core.Operators
-    
-    module Drum = 
-        let ammonia = { Size=PositiveSize.get 10m; Type=Ammonia } 
-        let tnt = { Size=PositiveSize.get 10m; Type=TNT } 
-        let biologicalSamples = { Size=PositiveSize.get 10m; Type=BiologicalSamples }
-        let sand = { Size=PositiveSize.get 10m; Type = Sand }
+﻿namespace DataContext
+open Domain
 
-    module Container = 
-        let big = Container.empty (PositiveSize.get 100m) Set.empty
+module Drum = 
+    let ammonia = { Size=PositiveSize.get 10m; Type=Ammonia } 
+    let tnt = { Size=PositiveSize.get 10m; Type=TNT } 
+    let biologicalSamples = { Size=PositiveSize.get 10m; Type=BiologicalSamples }
+    let sand = { Size=PositiveSize.get 10m; Type = Sand }
 
-    module ``when packing them, then expect no answer found`` =
-        let [<Fact>] ``one drum larger than the container`` () = [Container.big] |> Containers.pack [ { Drum.sand with Size = PositiveSize.get 101m } ] == None
+module Container = 
+    let big = Container.empty (PositiveSize.get 100m) Set.empty
+
+namespace ``Given drums and incompatible containers, `` 
+
+open Xunit
+open Domain
+open Core.Operators
+open DataContext
+
+module ``when packing them, then expect no answer found`` =
+    let [<Fact>] ``one drum larger than the container`` () = [Container.big] |> Containers.pack [ { Drum.sand with Size = PositiveSize.get 101m } ] == None
